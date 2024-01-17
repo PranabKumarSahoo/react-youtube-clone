86f30d8aef8197c320c5846cec6ff05ba02c2d73
PranabKumarSahoo <kumarspranab19@gmail.com>
2024-01-18 01:08:17 +0530

    [add]: fetched videos from API

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,8 +1,8 @@
-import React, { useContext, useEffect } from 'react'
+import React, { useContext, useEffect } from 'react';
 
 import { context } from '../context/contextApi';
-import LeftNav from './LeftNav'
-import VideoCard from './VideoCard'
+import LeftNav from './LeftNav';
+import VideoCard from './VideoCard';
 
 const Feed = () => {
 
@@ -19,9 +19,9 @@ const Feed = () => {
                 <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 p-5'>
                     {
                         !loading && searchResults &&
-                        searchResults.map((item) => {
+                        searchResults?.map((item) => {
 
-                            if (item.type !== "video") return false;
+                            if (item?.type !== "video") return false;
 
                             return (
                                 <VideoCard
